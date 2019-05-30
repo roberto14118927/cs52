cbbf4d4c04d55fd24855f88ee2d16a3ecaf207bf
roberto14118927 <robertoruiz141189@gmail.com>
2019-05-30 10:00:49 -0500

    add heroku

--- csaplication/settings.py
+++ csaplication/settings.py
@@ -8,8 +8,8 @@
 import os
 # Recusros de heroku
 import django_heroku
-import dj_database_url
 from decouple import config
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
